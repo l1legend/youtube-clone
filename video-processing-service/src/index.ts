579716619108bc189eda5fbe6937690b13462276
michael <michaelmin2020@gmail.com>
2025-01-30 14:24:33 -0600

    conditional to handle missing input and output file path

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -4,8 +4,15 @@ import ffmpeg from 'fluent-ffmpeg';
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.post("/process-video", (req, res) => {
+    // get path of the input video file from the request body
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send ("Bad request: Missing File path");
+    }
+
 });
 
 app.listen(port, () => {
