750dd3d2dca7f01d93f5c1c6a5669bc3764b9208
michael <michaelmin2020@gmail.com>
2025-01-30 15:02:43 -0600

    reformmated code

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,38 +1,37 @@
-import express from 'express';
-import ffmpeg from 'fluent-ffmpeg';
+import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
 
 app.post("/process-video", (req, res) => {
-    // get path of the input video file from the request body
-    const inputFilePath = req.body.inputFilePath;
-    const outputFilePath = req.body.outputFilePath;
+  // get path of the input video file from the request body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
 
-    //check if inputFilePath is missing
-    if (!inputFilePath) {
-        return res.status(400).send("Bad request: inputFilePath is missing");
-    }
+  //check if inputFilePath is missing
+  if (!inputFilePath) {
+    return res.status(400).send("Bad request: inputFilePath is missing");
+  }
 
-    // Check if outputFilePath is missing
-    if (!outputFilePath) {
-        return res.status(400).send("Bad request: outputFilePath is missing");
-    }
+  // Check if outputFilePath is missing
+  if (!outputFilePath) {
+    return res.status(400).send("Bad request: outputFilePath is missing");
+  }
 
-    ffmpeg(inputFilePath)
-        .outputOptions("-vf", "scale=-1:360") // 360p
-        .on("end", () => {
-        res.status(200).send("Video processing finished successfully");
-        })
-        .on("error", (err) => {
-            console.log(`An error occurred: ${err.message}`);
-            res.status(500).send(`Internal Server Error: ${err.message}`);
-        })
-        .save(outputFilePath);
-    
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360") // 360p
+    .on("end", () => {
+      res.status(200).send("Video processing finished successfully");
+    })
+    .on("error", (err) => {
+      console.log(`An error occurred: ${err.message}`);
+      res.status(500).send(`Internal Server Error: ${err.message}`);
+    })
+    .save(outputFilePath);
 });
 
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-    console.log(
-        `Video processing service listening at http://localhost:${port}`);
+  console.log(`Video processing service listening at http://localhost:${port}`);
 });
