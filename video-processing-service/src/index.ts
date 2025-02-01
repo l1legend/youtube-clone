6d44402627e10c481cd19b1967a28a09bbe0153c
michael <michaelmin2020@gmail.com>
2025-02-01 11:56:06 -0600

    added async functions for downloading and uploading raw video

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -13,24 +13,7 @@ app.post("/process-video", (req, res) => {
         res.status(400).send("Bad Request: Missing file path.");
     }
 
-    ffmpeg(inputFilePath)
-        .outputOptions("-vf", "scale=640:360") // Fixed dimensions
-        .outputOptions("-c:v", "libx264") // Specify video codec
-        .outputOptions("-c:a", "aac") // Specify audio codec
-        .on("start", (commandLine) => {
-            console.log(`FFmpeg process started with command: ${commandLine}`);
-        })
-        .on("end", () => {
-            res.status(200).send("Video processing finished successfully");
-        })
-        .on("error", (err) => {
-            console.log(`An error occurred: ${err.message}`);
-            res.status(500).send(`Internal Server Error: ${err.message}`);
-        })
-        .on("stderr", (stderrLine) => {
-            console.log(`FFmpeg stderr: ${stderrLine}`);
-        })
-        .save(outputFilePath);
+
 });
 
 const port = process.env.PORT || 3000;
---
+++ video-processing-service/src/storage.ts
@@ -0,0 +1,73 @@
+// 1. GCS file interactions
+// 2. GCS file interactions
+
+import { Storage } from '@google-cloud/storage';
+import fs from 'fs';
+import ffmpeg from 'fluent-ffmpeg';
+
+const storage = new Storage();
+
+const rawVideoBucketName = "mintwin-raw-videos";
+const processedVideoBucketName = "mintwin-processed-video";
+
+const localRawVideoPath = "./raw-videos";
+const localProcessedVideoPath = "./processed-videos";
+
+
+/**
+ * Creates the local directories for raw and processed videos.
+ */
+export function setupDirectories() {
+
+}
+
+/**
+ * @param rawVideoName - The name of the file to convert from {@link localRawVideoPath}.
+ * @param processedVideoName - The name of the file to convert to {@link localProcessedVideoPath}.
+ * @returns A promise that resolves when the video has been converted.
+ */
+
+export function convertVideo(rawVideoName: string, processedVideoName: string) {
+    return new Promise<void>((resolve, reject) => {
+        ffmpeg(`${localRawVideoPath}/${rawVideoName}`)
+        .outputOptions("-vf", "scale=640:360") // Fixed dimensions
+        .outputOptions("-c:v", "libx264") // Specify video codec (H.264)
+        .outputOptions("-c:a", "aac") // Specify audio codec 
+
+        .on("end", () => {
+            console.log("Video processing finished successfully");
+            resolve();
+        })
+
+        .on("error", (err) => {
+            console.log(`An error occurred: ${err.message}`);
+            reject(err);
+        })
+
+        .on("stderr", (stderrLine) => {
+            console.log(`FFmpeg stderr: ${stderrLine}`);
+        })
+        .save(`${localProcessedVideoPath}/${processedVideoName}`);
+    })
+}
+
+/**
+ * @param fileName - The name of the file to download from the 
+ * {@link rawVideoBucketName} bucket into the {@link localRawVideoPath} folder.
+ * @returns A promise that resolves when the file has been downloaded.
+ */
+export async function downloadRawVideo(fileName: string) {
+    await storage.bucket(rawVideoBucketName)
+        .file(fileName)
+        .download({ destination: `${localRawVideoPath}/${fileName}` });
+
+        console.log()
+}
+
+/**
+ * @param fileName - The name of the file to upload from the 
+ * {@link localProcessedVideoPath} folder into the {@link processedVideoBucketName}.
+ * @returns A promise that resolves when the file has been uploaded.
+ */
+export async function uploadProcessedVideo(fileName: string) {
+}
