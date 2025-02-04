eb1679eaf0a2893201b10f483d9e4e34728910c2
michael <michaelmin2020@gmail.com>
2025-02-03 20:20:45 -0600

    delete some boilerplate and css code

---
+++ yt-web-client/app/page.tsx
@@ -0,0 +1,17 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+
+      <main className={styles.main}>
+
+        <ol>
+          <li>
+            Get started by editing <code>app/page.tsx</code>.
+          </li>
+        </ol>
+
+      </main>
+ 
+  );
+}
