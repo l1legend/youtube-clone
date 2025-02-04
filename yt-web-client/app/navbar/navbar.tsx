e26797f9f92bc6b275f008bb6e1697b2b2aee840
michael <michaelmin2020@gmail.com>
2025-02-04 14:22:32 -0600

    fleshed out navbar component

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,7 +1,14 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './navbar.module.css';
+
 export default function Navbar() {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <nav className={styles.nav}>
+            <Link href="/">
+                    <Image width={90} height={20}
+                        src="youtube-logo.svg" alt="YouTube Logo" />
+            </Link>
+        </nav>
     );
 }
